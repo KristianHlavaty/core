daafd5e46ba05322537f5dbe69c2e8ead7f2b0d8
Kristian Hlavaty <khlavaty@c3r8s3.42prague.com>
2023-01-16 17:41:38 +0100

    first tested seems okey

---
+++ Libft/libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khlavaty <khlavaty@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 13:25:55 by khlavaty          #+#    #+#             */
+/*   Updated: 2023/01/16 17:32:46 by khlavaty         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int n);
+int	ft_isalnum(int cn);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif
