32a2f16ef9e1e36233a814ba3681c19782703cd7
Kristian Hlavaty <khlavaty@c3r9s1.42prague.com>
2023-02-09 22:36:23 +0100

    files

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,18 @@
 /*   By: khlavaty <khlavaty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 21:59:21 by khlavaty          #+#    #+#             */
-/*   Updated: 2023/02/09 22:05:26 by khlavaty         ###   ########.fr       */
+/*   Updated: 2023/02/09 22:22:23 by khlavaty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/types.h>
+
+char	*get_next_line(int fd);
+
+#endif
