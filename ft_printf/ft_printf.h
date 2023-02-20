3bbeb65d06890d20465acdeff20eee2d9081ce46
Kristian Hlavaty <khlavaty@c3r8s1.42prague.com>
2023-02-20 18:39:56 +0100

    just preparing files

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,17 @@
 /*   By: khlavaty <khlavaty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 18:11:09 by khlavaty          #+#    #+#             */
-/*   Updated: 2023/02/20 18:11:12 by khlavaty         ###   ########.fr       */
+/*   Updated: 2023/02/20 18:37:31 by khlavaty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *input, ...);
+
+#endif
