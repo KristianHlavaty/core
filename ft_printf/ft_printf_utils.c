0b09e0c433f86bff75607e2c248ba57099963788
Kristian Hlavaty <khlavaty@c3r8s1.42prague.com>
2023-02-20 18:44:58 +0100

    just preparing files

--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,8 +6,13 @@
 /*   By: khlavaty <khlavaty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 18:12:33 by khlavaty          #+#    #+#             */
-/*   Updated: 2023/02/20 18:37:59 by khlavaty         ###   ########.fr       */
+/*   Updated: 2023/02/20 18:42:57 by khlavaty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
