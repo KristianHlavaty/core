f238afcf736237e6b6227e6940153309b87cd6a1
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2023-02-27 18:36:09 +0100

    some errors with x will fix later lol

--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,11 +6,21 @@
 /*   By: khlavaty <khlavaty@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 18:12:33 by khlavaty          #+#    #+#             */
-/*   Updated: 2023/02/26 18:24:26 by khlavaty         ###   ########.fr       */
+/*   Updated: 2023/02/27 18:35:22 by khlavaty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
 
 int	ft_putchar(char c)
 {
@@ -81,4 +91,42 @@ int	ft_putnbrunsig(unsigned int n)
 		i += ft_putchar(c);
 	}
 	return (i);
+}
+
+int	ft_putbase(int n, char *base, int base_len)
+{
+	int	i;
+
+	i = 1;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		i++;
+		n = n * (-1);
+	}
+	if (n / base_len > 0)
+	{
+		i = i + ft_putbase(n / base_len, base, base_len);
+	}
+	ft_putchar(base[n % base_len]);
+	return (i);
+}
+
+int	ft_putnbrbase(int n, char *base)
+{
+	int	i;
+	int	base_len;
+
+	i = 0;
+	base_len = ft_strlen(base);
+	i = ft_putbase(n, base, base_len);
+	return (i);
+}
+
+int	ft_puthex(unsigned int n, char formt)
+{
+	if (formt == 'x')
+		return (ft_putnbrbase(n, "0123456789abcdef"));
+	else
+		return (ft_putnbrbase(n, "0123456789ABCDEF"));
 }
